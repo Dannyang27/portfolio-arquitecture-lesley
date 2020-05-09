1062ecf1efca129b0ce08e362e0d8b7915c93158
dannyang27 <danny27995@gmail.com>
2020-05-09 11:36:37 +0100

    Create link components

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import {BrowserRouter as Router, Link} from "react-router-dom";
+import {Switch, Route} from 'react-router';
+import Render from "./components/renders/Render";
 import './App.css';
+import Contact from "./components/contact/Contact";
+import About from "./components/about/About";
 
 function App() {
-  return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-            </ul>
-          </nav>
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Renders</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-          <Switch>
-            <Route path="/about">
-              <h2>test</h2>
-            </Route>
-            <Route path="/users">
-              <h3>Test 2</h3>
-            </Route>
-            <Route path="/">
-              <h3>test3</h3>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-  );
+                <Switch>
+                    <Route exact path="/">
+                        <Render/>
+                    </Route>
+                    <Route path="/about">
+                        <About/>
+                    </Route>
+                    <Route path="/contact">
+                        <Contact/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/renders/Render.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const render = () => {
+    return(
+        <h1>Renders</h1>
+    )
+}
+
+export default render;
