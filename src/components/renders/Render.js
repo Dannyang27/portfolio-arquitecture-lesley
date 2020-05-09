025dc6bb6e0a8e06ad646a8d840f4678a7df4ec6
dannyang27 <danny27995@gmail.com>
2020-05-09 12:01:46 +0100

    Add images to project

--- src/components/renders/Render.js
+++ src/components/renders/Render.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const render = () => {
+    const images = render_images(require.context('./images', false, /\.(png|jpe?g|svg)$/));
     return(
-        <h1>Renders</h1>
+        <h1>Renders: {images.length}</h1>
     )
+}
+
+const render_images = (r) => {
+    return r.keys().map(r);
 }
 
 export default render;
