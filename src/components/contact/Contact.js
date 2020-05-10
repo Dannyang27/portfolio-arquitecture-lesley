aa082c8444082b3a6f5b1a69d4d957eba07ae415
dannyang27 <danny27995@gmail.com>
2020-05-10 19:12:49 +0100

    Update instagram link

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,7 +5,7 @@ const contact = () => {
         <div className="information-block">
             <h4 className="helvetica-light size-medium light-grey">weil@weilvisual.com</h4>
             <h4 className="helvetica-light size-medium light-grey">+44 07500 569115</h4>
-            <a href="https://www.instagram.com/wlesley_yang/?hl=es" target="_blank">
+            <a href="https://www.instagram.com/weil_visual/" target="_blank">
                 <i className={"fa fa-instagram instagram-logo-medium"}></i>
             </a>
         </div>
