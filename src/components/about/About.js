94f4b02d1ba1372da4270445d3e627a22257a789
dannyang27 <danny27995@gmail.com>
2020-05-17 15:18:19 +0100

    Justify text from About page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,11 +7,11 @@ const About = () => {
         <div className="information-block">
             <img src={portraitImage} className="portrait-image-size my-float-left"/>
             <p className="p-left-align">
-                <h4 className="helvetica-light size-medium light-grey">
+                <h4 className="helvetica-light size-medium light-grey text-justify">
                     Wei L is an registered Architect in Spain based in London working currently for the renowned
                     office BIG as Junior Designer with previous experience working at MJZ and Sou Fujimoto Atelier.
                 </h4>
-                <h4 className="helvetica-light size-medium light-grey">
+                <h4 className="helvetica-light size-medium light-grey text-justify">
                     Recently he has decided to open his own practice to give voice to the yet unbuilt architecture,
                     focusing on clean cut visuals.
                 </h4>
