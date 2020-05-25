6841543d45f53387cb8c2334f133662392b9a2ab
dannyang27 <danny27995@gmail.com>
2020-05-25 10:23:11 +0100

    Implement active link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {BrowserRouter as Router, Link} from "react-router-dom";
 import {Switch, Route} from 'react-router';
 import Render from "./components/renders/Render";
@@ -8,7 +8,27 @@ import About from "./components/about/About";
 import 'bootstrap/dist/css/bootstrap.css';
 import logo from "./images/logo.png"
 
-function App() {
+const App = () => {
+
+    const currentLocation = window.location.pathname
+    let currentLocationId = 0;
+
+    switch (currentLocation) {
+        case "/":
+            currentLocationId = 0;
+            break;
+        case "/about":
+            currentLocationId = 1;
+            break;
+        case "/contact":
+            currentLocationId = 2;
+            break;
+        default:
+            currentLocationId = 0;
+    }
+
+    const [linkId, setLinkId] = useState(currentLocationId);
+
     return (
         <Router>
             <div className="app-container">
@@ -31,20 +51,23 @@ function App() {
 
                 <nav className="navbar navbar-top-margin">
                     <div>
-                        <ul class="navbar-nav">
+                        <ul className="navbar-nav">
                             <li>
-                                <Link to="/" className="link">
-                                    <h3 className="helvetica-light size-medium light-grey link-hover">Renders</h3>
+                                <Link to="/" className="link" onClick={() => setLinkId(0)}>
+                                    <h3 className="helvetica-light size-medium light-grey link-hover"
+                                        style={linkId === 0 ? {color: "black"} : {}}>Renders</h3>
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/about">
-                                    <h3 className="helvetica-light size-medium light-grey link-hover">About</h3>
+                                <Link to="/about" onClick={() => setLinkId(1)}>
+                                    <h3 className="helvetica-light size-medium light-grey link-hover"
+                                        style={linkId === 1 ? {color: "black"} : {}}>About</h3>
                                 </Link>
                             </li>
                             <li>
-                                <Link to="/contact">
-                                    <h3 className="helvetica-light size-medium light-grey link-hover">Contact</h3>
+                                <Link to="/contact" onClick={() => setLinkId(2)}>
+                                    <h3 className="helvetica-light size-medium light-grey link-hover"
+                                        style={linkId === 2 ? {color: "black"} : {}}>Contact</h3>
                                 </Link>
                             </li>
                         </ul>
